2729077e7324de00c21d611738db016033e6b970
kash-jay <kp2681@nyu.edu>
2023-09-13 13:52:07 +0400

    First commit

---
+++ cast.js
@@ -0,0 +1,22 @@
+backButton = document.getElementById('back-wrapper');
+actorInfoSections = document.querySelectorAll('.actor-info');
+
+backButton.onclick = function () {
+    window.location.href = 'index.html';
+}
+
+function scrollAnim() {
+    for (var i = 0; i <actorInfoSections.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = actorInfoSections[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+        if (elementTop < windowHeight - elementVisible) {
+            actorInfoSections[i].classList.add('active');
+        } else {
+            actorInfoSections[i].classList.remove('active');
+        }
+    }
+}
+
+window.addEventListener("scroll", scrollAnim);
+scrollAnim();
---
+++ main.js
@@ -0,0 +1,56 @@
+castButton = document.getElementById('cast-wrapper');
+tipButtons = document.querySelectorAll('.tip-button');
+video = document.getElementById('video');
+
+castButton.onclick = function () {
+    window.location.href = 'cast.html';
+}
+
+function scrollAnim() {
+    for (var i = 0; i <tipButtons.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = tipButtons[i].getBoundingClientRect().top;
+        var elementVisible = 50;
+        if (elementTop < windowHeight - elementVisible) {
+            tipButtons[i].classList.add('active');
+        } else {
+            tipButtons[i].classList.remove('active');
+        }
+    }
+}
+
+window.addEventListener("scroll", scrollAnim);
+scrollAnim();
+
+window.addEventListener('DOMContentLoaded', () => {
+    video.classList.add('anim');
+})
+
+modalActive = 1
+
+tipButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        num = button.id;
+        console.log(num);
+        modal = document.getElementById(`modal-${num}`);
+        modalOpen = document.getElementById(`modal-${modalActive}`);
+        modalOpen.style.opacity = '0';
+        modalOpen.style.display = 'none';
+        modalOpen.style.zIndex = '0';
+        modal.style.display = 'flex';
+        modal.style.opacity = '1';
+        modal.style.zIndex = '2';
+        console.log(modalActive);
+        modalActive = num;
+    })
+});
+
+closeBtns = document.querySelectorAll('.close-buttn');
+closeBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        modalOpen = document.getElementById(`modal-${modalActive}`);
+        modalOpen.style.display = 'none';
+        modalOpen.style.opacity = '0';
+        modalOpen.style.zIndex = '0';
+    })
+});
